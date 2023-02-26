73ac321f2d4d79b0631f3a96141e16abe9bdb1ca
Romain Spielmann <79088864+romain-spielmann@users.noreply.github.com>
2023-02-26 18:29:45 +0100

    add dark theme provider

--- src/provider/GameProvider.jsx
+++ src/provider/GameProvider.jsx
@@ -1,37 +1,35 @@
 import {createContext, useContext, useState} from "react";
 
-const ColorJoueurXValue = createContext(undefined, undefined);
-const ColorJoueurXSetter = createContext(undefined, undefined);
+const ColorJoueurXValue = createContext();
+const ColorJoueurXSetter = createContext();
 
-const ColorJoueur0Value = createContext(undefined, undefined);
-const ColorJoueur0Setter = createContext(undefined, undefined);
+const ColorJoueur0Value = createContext();
+const ColorJoueur0Setter = createContext();
 
-const ColorBackgroundValue = createContext(undefined, undefined);
-const ColorBackgroundSetter = createContext(undefined, undefined);
-
-const ColorBackgroundTxtValue = createContext(undefined, undefined);
-const ColorBackgroundTxtSetter = createContext(undefined, undefined);
+const IsDarkThemeSetter = createContext();
+const ColorBackgroundValue = createContext();
+const ColorBackgroundTxtValue = createContext();
 
 export function GameProvider({ children }) {
     const [colorJoueurX, setColorJoueurX] = useState('#87e7e1');
     const [colorJoueur0, setColorJoueur0] = useState('#e8b5db');
-    const [colorBackground, setColorBackground] = useState('#121212');
-    const [colorBackgroundTxt, setColorBackgroundTxt] = useState('white');
+    const [isDarkTheme, setIsDarkTheme] = useState(true);
+    
+    const colorBackground = isDarkTheme ? '#121212' : 'white'
+    const colorBackgroundTxt = isDarkTheme ? 'white' : '#121212'
 
     return (
         <ColorJoueurXSetter.Provider value={setColorJoueurX}>
             <ColorJoueurXValue.Provider value={colorJoueurX}>
                 <ColorJoueur0Setter.Provider value={setColorJoueur0}>
                     <ColorJoueur0Value.Provider value={colorJoueur0}>
-                        <ColorBackgroundSetter.Provider value={setColorBackground}>
-                            <ColorBackgroundValue.Provider value={colorBackground}>
-                                <ColorBackgroundTxtSetter.Provider value={setColorBackgroundTxt}>
-                                    <ColorBackgroundTxtValue.Provider value={colorBackgroundTxt}>
-                                        {children}
-                                    </ColorBackgroundTxtValue.Provider>
-                                </ColorBackgroundTxtSetter.Provider>
-                            </ColorBackgroundValue.Provider>
-                        </ColorBackgroundSetter.Provider>
+                        <ColorBackgroundValue.Provider value={colorBackground}>
+                            <ColorBackgroundTxtValue.Provider value={colorBackgroundTxt}>
+                                <IsDarkThemeSetter.Provider value={setIsDarkTheme}>
+                                    {children}
+                                </IsDarkThemeSetter.Provider>
+                            </ColorBackgroundTxtValue.Provider>
+                        </ColorBackgroundValue.Provider>
                     </ColorJoueur0Value.Provider>
                 </ColorJoueur0Setter.Provider>
             </ColorJoueurXValue.Provider>
@@ -59,14 +57,10 @@ export function useColorBackgroundValue() {
     return useContext(ColorBackgroundValue);
 }
 
-export function useColorBackgroundSetter() {
-    return useContext(ColorBackgroundSetter);
-}
-
 export function useColorBackgroundTxtValue() {
     return useContext(ColorBackgroundTxtValue);
 }
 
-export function useColorBackgroundTxtSetter() {
-    return useContext(ColorBackgroundTxtSetter);
+export function useSetDarkTheme() {
+    return useContext(IsDarkThemeSetter)
 }
